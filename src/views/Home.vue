d9e256929a87bad82ccb6ca6e70f31f852e0d150
Michael Scarbrough <michael.scarbrough@nutanix.com>
2020-06-20 12:58:07 -0400

    Updated the Welcome message to rotate titles.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <Welcome />
+    <Results />
   </div>
 </template>
 
 <script>
 import Welcome from '@/components/Welcome.vue'
+import Results from '@/components/Results.vue'
 
 export default {
   components: {
-    Welcome
+    Welcome,
+    Results
   }
 }
 </script>
