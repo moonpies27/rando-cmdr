ac0a552606df46b4430356edf277026d9486c13a
Michael Scarbrough <michael.scarbrough@nutanix.com>
2020-06-21 18:48:10 -0400

    Added a footer component and styled it.

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center bg-gray-400 sticky bottom-0 w-full py-2 text-sm">
+    <p>This app makes use of the <a href="https://scryfall.com/docs/api" target="__blank" class="underline">Scryfall API</a>.  Card images are &copy;Wizards of the Coast, LLC.</p>
+  </div>
+</template>
