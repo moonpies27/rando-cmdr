29a0ccc7ae37edb90f830c9bcf14d33f6c5674be
Mike Scarbrough <43490635+moonpies27@users.noreply.github.com>
2020-06-21 22:54:59 -0400

    Development (#9)
    * Fixed a typo
    
    * Added a footer component and styled it. (#7)
    
    Co-authored-by: Michael Scarbrough <michael.scarbrough@nutanix.com>
    
    * Add filters (#8)
    
    * Added state values for filters and styled checkboxes.
    
    * Implemented filter logic
    
    * Changed card image size in results component
    
    Co-authored-by: Michael Scarbrough <michael.scarbrough@nutanix.com>
    
    Co-authored-by: Michael Scarbrough <michael.scarbrough@nutanix.com>

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,39 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     players: [],
-    cards: []
+    cards: [],
+    filters: [
+      {
+        label: 'Include Vanilla',
+        desc: 'Include creatures that don\'t have abilities.',
+        value: 'includeVanilla'
+      },
+      {
+        label: 'Include French-Vanilla',
+        desc: 'Include creatures that only have evergreen abilities such as Flying, Trample, or Morph.',
+        value: 'includeFrenchVanilla'
+      },
+      {
+        label: 'Include Planeswalkers',
+        desc: 'Include all planeswalkers in the result set.',
+        value: 'includePlaneswalkers'
+      },
+      {
+        label: 'Include Banned Commanders',
+        desc: 'Include legendary creatures that appear on the official Commander ban list.',
+        value: 'includeBanned'
+      },
+      {
+        label: 'Include Silver Border',
+        desc: 'Include legendary creatures and/or planeswalkers from silver-bordered sets.',
+        value: 'includeSilveBorder'
+      },
+      {
+        label: 'Include Flip Cards',
+        desc: 'Include the Flip cards from Kamigawa block.',
+        value: 'includeFip'
+      }
+    ]
   },
   mutations: {
     addPlayer (state, newPlayer) {
